a9a4555456334f127e699ab8370fa50139340610
klekkwedge <antonov.andrey.10101@gmail.com>
2022-07-26 08:50:01 +0300

    feat: add Main component

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,7 +1,24 @@
+import { Flex } from "@chakra-ui/react";
 import "./App.css";
-
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+// font-family: "Noto Sans Display",sans-serif;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Flex
+      flexDirection="column"
+      gap="20px"
+      background="#db524d"
+      color="white"
+      width="100vw"
+      height="100vh"
+      overflow='hidden'
+      alignItems='center'
+    >
+      <Header />
+      <Main />
+    </Flex>
+  );
 }
 
 export default App;
---
+++ pomodoro/src/components/Main/Main.js
@@ -0,0 +1,66 @@
+import { Flex, Button, Heading } from "@chakra-ui/react";
+
+const Main = () => {
+  return (
+    <Flex
+      flexDirection="column"
+      background="#DF645F"
+      padding="20px 80px"
+      alignItems="center"
+      borderRadius='10px'
+    >
+      <Flex gap="15px">
+        <Button
+          fontSize="18px"
+          width="100px"
+          height="28px"
+          background="transparent"
+          border="none"
+          color="white"
+          cursor="pointer"
+        >
+          Pomodoro
+        </Button>
+        <Button
+          fontSize="18px"
+          width="100px"
+          height="28px"
+          background="transparent"
+          border="none"
+          color="white"
+          cursor="pointer"
+        >
+          Short Break
+        </Button>
+        <Button
+          fontSize="18px"
+          width="100px"
+          height="28px"
+          background="transparent"
+          border="none"
+          color="white"
+          cursor="pointer"
+        >
+          Long Break
+        </Button>
+      </Flex>
+      <Heading fontSize="120px" margin="20px 0px">
+        25:00
+      </Heading>
+      <Button
+        fontSize="40px"
+        borderRadius='10px'
+        fontWeight='500'
+        width="200px"
+        height="55px"
+        background="white"
+        border="none"
+        color="#DC665F"
+        cursor="pointer"
+      >
+        Start
+      </Button>
+    </Flex>
+  );
+};
+export default Main;
