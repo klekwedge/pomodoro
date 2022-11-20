c0e502143d43ca833101f78c82dc16421a54dcf9
Andrey <antonov.andrey.10101@gmail.com>
2022-11-20 15:07:55 +0300

    feat: add progress drawing in details

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -16,6 +16,7 @@ const Detail = () => {
           color="gray"
           borderBottom="1px solid gray"
           pb="10px"
+          mb="10px"
         >
           <Heading as="h4" fontSize="14px" fontWeight="400">
             DATE
@@ -24,6 +25,25 @@ const Detail = () => {
             MINUTES
           </Heading>
         </Flex>
+        {progress.length > 0
+          ? progress.map((item) => (
+              <Flex
+                alignItems="center"
+                justifyContent="space-between"
+                color="gray"
+                pb="10px"
+                mb="10px"
+                key={item.id}
+              >
+                <Heading as="h4" fontSize="14px" fontWeight="400">
+                  {`${item.date}`}
+                </Heading>
+                <Heading as="h4" fontSize="14px" fontWeight="400">
+                  {item.minutes}
+                </Heading>
+              </Flex>
+            ))
+          : null}
       </Flex>
     </TabPanel>
   );
